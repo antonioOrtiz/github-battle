1e5f4e4f6ef76d777ee1f0d54311946f93c5ab3c
Antonio Pavicevac-Ortiz <17antonio.ortiz@gmail.com>
2018-04-22 16:43:01 -0400

    destructured properties on props and state object and arrow functions and short-hand properties on objects

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -3,51 +3,47 @@ import PropTypes from 'prop-types';
 import api from '../utils/api';
 import Loading from './Loading';
 
-function SelectLangauge(props) {
+// prettier-ignore
+function SelectLangauge({ selectedLanguage, onSelect }) {
+  /* destructuring props */
   var languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python'];
 
   return (
     <ul className="languages">
-      {languages.map(function(lang) {
-        return (
-          <li
-            style={lang == props.selectedLanguage ? { color: '#d0021b' } : null}
-            onClick={props.onSelect.bind(null, lang)}
-            key={lang}
-          >
-            {lang}
-          </li>
-        );
-      })}
+      {languages.map(lang => (   /* leveraging arrow functions implicit return */
+        <li style={lang == selectedLanguage ? { color: '#d0021b' } : null}
+            onClick={() => onSelect(lang)}
+            key={lang}>
+          {lang}
+        </li>
+      ))}
     </ul>
   );
 }
 
 // prettier-ignore
-function RepoGrid(props) {
+function RepoGrid({repos}) {
   return (
     <ul className="popular-list">
-      {props.repos.map(function(repo, index) {
-        return (
-          <li key={repo.name} className="popular-item">
+      {repos.map(({name, stargazers_count, owner, html_url}, index) => ( /* destructuring */
+        <li key={name} className="popular-item">
             <div className="popular-rank">#{index + 1}</div>
             <ul className="space-list-items">
               <li>
                 <img
                   className="avatar"
-                  src={repo.owner.avatar_url}
-                  alt={'Avatar for ' + repo.owner.login}
+                  src={owner.avatar_url}
+                  alt={'Avatar for ' + owner.login}
                 />
               </li>
               <li>
-                <a href={repo.html_url}>{repo.name}</a>
+                <a href={html_url}>{name}</a>
               </li>
-              <li>@{repo.owner.login}</li>
-              <li>{repo.stargazers_count} stars</li>
+              <li>@{owner.login}</li>
+              <li>{stargazers_count} stars</li>
             </ul>
           </li>
-        );
-      })}
+      ))}
     </ul>
   );
 }
@@ -78,34 +74,26 @@ export default class Popular extends Component {
   }
 
   // prettier-ignore
-  updateLanguage(lang) {
-    this.setState(function() {
-      return {
+  updateLanguage(lang) { /* destructuring *note when returning a object you have to wrap in parens */
+    this.setState(() => ({
         selectedLanguage: lang,
         repos: null,
-      };
-    });
+    }));
 
     api.fetchPopularRepos(lang)
-    .then(function(repos) {
-      this.setState(function(){
-        return {
-          repos: repos
-        }
-      })
-    }.bind(this));
+    .then((repos) => this.setState(()=> ({ repos }))); /* when key and value name are the same, you can ommit the value*/
   }
 
   // prettier-ignore
   render() {
-
+    var {selectedLanguage, repos} = this.state /* destructuring off state*/
     return (
       <div>
-      <SelectLangauge selectedLanguage={this.state.selectedLanguage} onSelect={this.updateLanguage}/>
+      <SelectLangauge selectedLanguage={selectedLanguage} onSelect={this.updateLanguage}/>
 
       {!this.state.repos
          ? <Loading/>
-         : <RepoGrid repos={this.state.repos} />
+         : <RepoGrid repos={repos} />
         }
       </div>
     );
